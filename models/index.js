b361ba4a25c8b3eb5f3c0f866c9bd58d470cfcaa
ryanmuhl <ryanmuhl@hotmail.com>
2022-05-06 16:22:18 -0400

    attempt to seed database

--- models/index.js
+++ models/index.js
@@ -3,17 +3,17 @@ const Category = require('./category');
 const Item = require('./items');
 
 
-Item.hasOne(Category, {
-  foreignKey: 'category_id'
-})
+// Item.hasOne(Category, {
+//   foreignKey: 'category_id'
+// })
 
-User.hasMany(Item,{
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE',
-})
+// User.hasMany(Item,{
+//     foreignKey: 'user_id',
+//     onDelete: 'CASCADE',
+// })
 
-Item.belongsTo(User, {
-    foreignKey: 'user_id',
-})
+// Item.belongsTo(User, {
+//     foreignKey: 'user_id',
+// })
 
 module.exports = {User, Category, Item};
--- seeds/seed.js
+++ seeds/seed.js
@@ -5,12 +5,16 @@ const seedUser = require('./userData');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
+  console.log('--------------');
 
   await seedCategories();
+  console.log('--------------');
 
   await seedItems();
+  console.log('--------------');
 
   await seedUser();
+  console.log('--------------');
 
   process.exit(0);
 };
