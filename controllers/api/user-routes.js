356791a88c853efb44a2b97047825bfdeaab2e32
ryanmuhl <ryanmuhl@hotmail.com>
2022-05-07 19:15:21 -0400

    update user route to get user by id

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -13,6 +13,27 @@ router.get('/', (req, res) => {
     });
 });
 
+//get user by id
+router.get('/:id', (req, res) => {
+  User.findOne({
+    attributes: { exclude: ['password'] },
+    where: {
+      id: req.params.id
+    },
+  })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 //POST /api/user/
 router.post('/', (req, res) => {
   User.create({
