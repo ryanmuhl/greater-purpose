beac5fbd86b2531aafc8aa7ea41afb27525f5338
ryanmuhl <ryanmuhl@hotmail.com>
2022-05-10 20:31:20 -0400

    Update home.handlebars and login.handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,30 +1,9 @@
 const router = require("express").Router();
-const { Item, Category } = require("../models/");
+const { User, Item, Category } = require("../models/");
 
-// GET all galleries for homepage
-router.get('/', async (req, res) => {
-  try {
-    const dbItemData = await Item.findAll({
-      include: [
-        {
-          model: Category,
-          attributes: ['item_type'],
-        },
-      ],
-    });
-
-    const items = dbItemData .map((item) =>
-      item.get({ plain: true })
-    );
-    // Send over the 'loggedIn' session variable to the 'homepage' template
-    res.render('homepage', {
-      items,
-      loggedIn: req.session.loggedIn,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
+// homepage
+router.get("/", (req, res) => {
+  res.render("home");
 });
 
 // Login route
